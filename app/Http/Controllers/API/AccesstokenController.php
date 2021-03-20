068e1e8e81b76e1dcb477d3ceebbf5feacd876d0
fullstackmspl <fullstack.mspl@gmail.com>
2021-03-20 18:17:54 +0530

    data

---
+++ app/Http/Controllers/API/AccesstokenController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller\API;
+use Illuminate\Http\Request;
+use Twilio\Jwt\AccessToken;
+use Twilio\Jwt\Grants\VideoGrant;
+
+class AccesstokenController extends Controller
+{
+
+	public function test(Request $request){
+		echo"ddd";
+	}
+
+	
+    public function generate_token(Request $request)
+    {
+        // Substitute your Twilio Account SID and API Key details
+        $accountSid = env('TWILIO_ACCOUNT_SID');
+        $apiKeySid = env('TWILIO_API_KEY_SID');
+        $apiKeySecret = env('TWILIO_API_KEY_SECRET');
+
+        $identity = uniqid();
+
+        // Create an Access Token
+        $token = new AccessToken(
+            $accountSid,
+            $apiKeySid,
+            $apiKeySecret,
+            3600,
+            $identity
+        );
+
+        // Grant access to Video
+        $grant = new VideoGrant();
+        $grant->setRoom('cool room');
+        $token->addGrant($grant);
+
+        // Serialize the token as a JWT
+        echo $token->toJWT();
+    }
+}
